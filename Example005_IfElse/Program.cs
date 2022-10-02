f4a893f905c3abbecddd138184a113e3d1483871
Nadya <19nadya9425@gmail.com>
2022-10-02 13:31:08 +0300

    Example005_IfElse

---
+++ Example005_IfElse/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите Ваше имя: ");
+string username = Console.ReadLine();
+if(username.ToLower() == "надежда"){
+    Console.WriteLine("Ура, это Надежда!");
+}
+else {
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
